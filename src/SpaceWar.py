b24814d84e252d65748bc2d489f67784bfe74fc4
Pradyuman Iyer <pradyuman.iyer04@gmail.com>
2024-05-01 04:31:15 -0500

    Added movement to the player object with arrow controls

--- src/SpaceWar.py
+++ src/SpaceWar.py
@@ -8,11 +8,10 @@ turtle.bgcolor("black")
 turtle.ht()
 turtle.setundobuffer(1)
 turtle.tracer(1)
-turtle.mainloop()
 
 class Sprite(turtle.Turtle):
     def __init__(self, spriteshape, color, startx, starty):
-        turtle.Turtle.__init__(self, shape = spriteshape)
+        turtle.Turtle.__init__(self, shape=spriteshape)
         self.speed(0)
         self.penup()
         self.color(color)
@@ -20,10 +19,37 @@ class Sprite(turtle.Turtle):
         self.goto(startx, starty)
         self.speed = 1
 
+    def move(self):
+        self.fd(self.speed)
+
+class Player(Sprite):
+    def __init__(self, spriteshape, color, startx, starty):
+        Sprite.__init__(self, spriteshape, color, startx, starty)
+        self.speed = 4
+        self.lives = 3
+    
+    def turn_left(self):
+        self.lt(45)
+
+    def turn_right(self):
+        self.rt(45)
+
+    def accelerate(self):
+        self.speed += 1
+
+    def decelerate(self):
+        self.speed -= 1
 
 # Create Sprites
-player = Sprite("triangle", "white", 0, 0)
+player = Player("triangle", "white", 0, 0)
 
+# Keyboard Bindings
+turtle.onkey(player.turn_left, "Left")
+turtle.onkey(player.turn_right, "Right")
+turtle.onkey(player.accelerate, "Up")
+turtle.onkey(player.decelerate, "Down")
+turtle.listen()
+
+# Main Game Loop
 while True: 
-    player.fd(player.speed)
-    
+    player.move()
