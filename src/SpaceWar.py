a947574826dd33f3a0810e0e7c8ef7d6fe55854a
Pradyuman Iyer <pradyuman.iyer04@gmail.com>
2024-05-01 06:17:52 -0500

    Added a missile which destroys the enemy sprite and relaods after

--- src/SpaceWar.py
+++ src/SpaceWar.py
@@ -39,8 +39,8 @@ class Sprite(turtle.Turtle):
     def is_collision(self, other):
         if (self.xcor() >= (other.xcor() - 20)) and \
         (self.xcor() <= (other.xcor() + 20)) and \
-        (self.xcor() >= (other.ycor() - 20)) and \
-        (self.xcor() <= (other.ycor() + 20)):
+        (self.ycor() >= (other.ycor() - 20)) and \
+        (self.ycor() <= (other.ycor() + 20)):
             return True
         else: 
             return False
@@ -70,6 +70,32 @@ class Enemy(Sprite):
         self.speed = 6
         self.setheading(random.randint(0, 360))
 
+class Missile(Sprite):
+    def __init__(self, spriteshape, color, startx, starty):
+        Sprite.__init__(self, spriteshape, color, startx, starty)
+        self.shapesize(stretch_wid=0.3, stretch_len=0.4, outline=None)
+        self.speed = 20
+        self.status = "ready"
+
+    def fire(self):
+        if self.status == "ready":
+            self.goto(player.xcor(), player.ycor())
+            self.setheading(player.heading())
+            self.status = "firing"
+
+    def move(self):
+
+        if self.status == "ready":
+            self.goto(-1000, 1000)
+
+        if self.status == "firing":
+            self.fd(self.speed)
+
+        # Border Check
+        if self.xcor() < -290 or self.xcor() > 290 or \
+        self.ycor() < -290 or self.ycor() > 290:
+            self.goto(-1000, 1000)
+            self.status = "ready"
 
 class Game():
     def __init__(self):
@@ -102,21 +128,31 @@ game.draw_border()
 # Create Sprites
 player = Player("triangle", "white", 0, 0)
 enemy = Enemy("circle", "red", -100, 0)
+missile = Missile("triangle", "yellow", 0, 0)
 
 # Keyboard Bindings
 turtle.onkey(player.turn_left, "Left")
 turtle.onkey(player.turn_right, "Right")
 turtle.onkey(player.accelerate, "Up")
 turtle.onkey(player.decelerate, "Down")
+turtle.onkey(missile.fire, "space")
 turtle.listen()
 
 # Main Game Loop
 while True: 
     player.move()
     enemy.move()
+    missile.move()
 
     # Check for collisions
     if player.is_collision(enemy):
         x = random.randint(-250, 250)
         y = random.randint(-250, 250)
         enemy.goto(x, y)
+
+    # Checl for collisions between enemy and missile
+    if missile.is_collision(enemy):
+        x = random.randint(-250, 250)
+        y = random.randint(-250, 250)
+        enemy.goto(x, y)
+        missile.status = "ready"
